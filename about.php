c5516f4328c8b9d4346e4bc52d6108eb9a1efa8f
CedricBruges <cbruges@stlprioryschool.org>
2018-10-06 20:44:34 -0500

    Matching webhost and local files

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>The shust - About Us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../test.css" type="text/css">
+  </head>
+  <body>
+
+     <div class="jumbotron" style="margin-bottom:0">
+       <h1 class="display-4">The Shust</h1>
+       <p class="lead">Check out our products at the Shust.</p>
+     </div>
+<?php include '../header.php'?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!-- NAV BAR used in other documents -->
+<nav class="navbar navbar-expand-sm bg-light navbar-primary">
+       <a class="navbar-brand" href="#">The Shust</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="collapsibleNavbar">
+         <ul class="navbar-nav">
+           <li class="nav-item">
+             <a class="nav-link" href="http://www.theshust.me/">Home</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="http://www.theshust.me/about">About Us</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="http://www.theshust.me/product">Products</a>
+           </li>
+         </ul>
+       </div>
+     </nav>
