3cd9c39c46667dcb10f310e18bdf64ec4b6f3c9a
CedricBruges <cbruges@stlprioryschool.org>
2018-10-07 13:38:08 -0500

    favicon, collapsible and filled border with text w/ bootstrap

--- about.php
+++ about.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <?php include'../favicon.php'; ?>
     <title>Shust Solutions - About Us</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,3 +15,19 @@
        <p class="lead">Why Shust?</p>
      </div>
 <?php include '../header.php'?>
+<div class="border border-dark rounded p-4 m-5 bg h-50" style="background-color: #eee; width:1000px;">
+   <h1 class="display-4">This site is currently under development</h1>
+   <p class="lead">Contact for more information </p>
+   <br>
+   <h3>About tab </h3>
+
+
+
+<button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">Collapsible</button>
+<div id="demo" class="collapse">
+Collapsible successfully implemented with jquery and popper added.
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
